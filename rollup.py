569e9100410500317cae0daeb9f540432e1f40f7
Arno Hautala <arno@alum.wpi.edu>
2012-12-15 10:55:59 -0500

    added arbitrary interval support and argument definition. also cleaned up TODOs and TEST

--- rollup.py
+++ rollup.py
@@ -13,15 +13,13 @@
 # Also prune empty snapshots (0 bytes used) that aren't the most recent for any dataset
 
 # TODO: 
-#   arguments for configuring snapshots to keep (# daily, # weekly, etc)
-#   define arbitrary intervals (daily = 86400, quarter-hour = 900, etc)
-#   configuration file support
+#   configuration file support (?)
 #   rollup based on local time, not UTC
 #     requires pytz, or manually determining and converting time offsets
-#   arguments to select intervals, modify existing intervals, add new intervals
+#   improve documentation
 
 # TEST:
-#   arbitrary intervals !!!! current code untested
+#   arbitrary intervals (looks good so far)
 
 import datetime
 import calendar
@@ -38,7 +36,19 @@ intervals['weekly']  = { 'max' :  0, 'abbreviation':'w', 'reference' : '%Y-%W' }
 intervals['monthly'] = { 'max' : 12, 'abbreviation':'m', 'reference' : '%Y-%m' }
 intervals['yearly']  = { 'max' : 10, 'abbreviation':'y', 'reference' : '%Y' }
 
-used_intervals = [ 'hourly', 'daily', 'weekly' ]
+modifiers = {
+    'h' : 60,
+    'd' : 60*24,
+    'w' : 60*24*7,
+    'm' : 60*24*28,
+    'y' : 60*24*365,
+}
+
+used_intervals = {
+    'hourly': intervals['hourly'],
+    'daily' : intervals['daily'],
+    'weekly': intervals['weekly']
+}
 
 parser = argparse.ArgumentParser(description='Prune excess snapshots, keeping hourly for the last day, daily for the last week, and weekly thereafter.')
 parser.add_argument('datasets', nargs='+', help='the root dataset(s) from which to prune snapshots')
@@ -46,11 +56,44 @@ parser.add_argument('--test', '-t', action="store_true", default=False, help='on
 parser.add_argument('--verbose', '-v', action="store_true", default=False, help='display verbose information about which snapshots are kept, pruned, and why')
 parser.add_argument('--empty', '-z', action="store_true", default=False, help='prune empty snashots (will still retain the most recent even if empty)')
 parser.add_argument('--recursive', '-r', action="store_true", default=False, help='recursively pruning snapshots from nested datasets')
+parser.add_argument("--intervals", "-i", 
+    help="modify existing and define new snapshot intervals. either name existing intervals "+
+    "("+", ".join(intervals.keys())+"), modify the number of those to store (hourly:12), "+
+    "or define new intervals according to interval:count (2h:12). Multiple intervals may be "+
+    "specified if comma seperated (hourly,daily:30,2h12)."
+)
 
 args = parser.parse_args()
 
 if args.test:
     args.verbose = True
+
+if args.intervals:
+    used_intervals = {}
+    
+    for interval in args.intervals.split(','):
+        if interval.count(':') == 1:
+            period,count = interval.split(':')
+            
+            if period in intervals:
+                used_intervals[period] = intervals[period]
+                used_intervals[period]['max'] = count
+                
+            else:
+                if period[-1] in modifiers:
+                    used_intervals[interval] = { 'max' : count, 'interval' : int(period[:-1]) * modifiers[period[-1]] }
+                    
+                elif int(period):
+                    used_intervals[interval] = { 'max' : count, 'interval' : int(period) }
+                    
+        elif interval.count(':') == 0 and interval in intervals:
+            used_intervals[interval] = intervals[interval]
+
+for interval in used_intervals:
+    if 'abbreviation' not in used_intervals[interval]:
+        used_intervals[interval]['abbreviation'] = interval
+
+print used_intervals
 
 now = datetime.datetime.utcnow()
 
@@ -95,14 +138,21 @@ for dataset in sorted(snapshots.keys()):
 
         epoch = snapshots[dataset][snapshot]['creation']
         
-        for interval in used_intervals:
-            reference = time.strftime(intervals[interval]['reference'], time.gmtime(float(epoch)))
+        for interval in used_intervals.keys():
+            if 'reference' in used_intervals[interval]:
+                reference = time.strftime(used_intervals[interval]['reference'], time.gmtime(float(epoch)))
+                
+                if reference not in rollup_intervals[interval]:
+                    if used_intervals[interval]['max'] != 0 and len(rollup_intervals[interval]) > int(used_intervals[interval]['max']):
+                        rollup_intervals[interval].pop(sorted(rollup_intervals[interval].keys())[0])
+                    rollup_intervals[interval][reference] = epoch
             
-            if reference not in rollup_intervals[interval]:
-                if intervals[interval]['max'] != 0 and len(rollup_intervals[interval]) > intervals[interval]['max']:
+            elif 'interval' in used_intervals[interval]:
+                if used_intervals[interval]['max'] != 0 and len(rollup_intervals[interval]) > int(used_intervals[interval]['max']):
                     rollup_intervals[interval].pop(sorted(rollup_intervals[interval].keys())[0])
-                rollup_intervals[interval][reference] = epoch
-        
+                
+                if (not rollup_intervals[interval]) or int(sorted(rollup_intervals[interval].keys())[-1]) + (used_intervals[interval]['interval']*60*.9) < int(epoch):
+                    rollup_intervals[interval][epoch] = epoch
         
     for snapshot in sorted_snapshots:
         # enforce that this is an automated snapshot (presence of 'auto')
@@ -113,20 +163,31 @@ for dataset in sorted(snapshots.keys()):
         
         epoch = snapshots[dataset][snapshot]['creation']
         
-        for interval in used_intervals:
-            reference = time.strftime(intervals[interval]['reference'], time.gmtime(float(epoch)))
-            if reference in rollup_intervals[interval]:
-                prune = False
+        for interval in used_intervals.keys():
+            if 'reference' in used_intervals[interval]:
+                reference = time.strftime(used_intervals[interval]['reference'], time.gmtime(float(epoch)))
+                if reference in rollup_intervals[interval] and rollup_intervals[interval][reference] == epoch:
+                    prune = False
+                    
+            elif 'interval' in used_intervals[interval]:
+                if epoch in rollup_intervals[interval]:
+                    prune = False
 
         if prune or args.verbose:
             print "\t","pruning\t" if prune else " \t", "@"+snapshot, 
             if args.verbose:
-                for interval in used_intervals:
-                    reference = time.strftime(intervals[interval]['reference'], time.gmtime(float(epoch)))
-                    if rollup_intervals[interval][reference] == epoch:
-                        print intervals[interval]['abbreviation'],
-                    else:
-                        print '-',
+                for interval in used_intervals.keys():
+                    if 'reference' in used_intervals[interval]:
+                        reference = time.strftime(used_intervals[interval]['reference'], time.gmtime(float(epoch)))
+                        if reference in rollup_intervals[interval] and rollup_intervals[interval][reference] == epoch:
+                            print used_intervals[interval]['abbreviation'],
+                        else:
+                            print '-',
+                    if 'interval' in used_intervals[interval]:
+                        if epoch in rollup_intervals[interval]:
+                            print used_intervals[interval]['abbreviation'],
+                        else:
+                            print '-',
                 print snapshots[dataset][snapshot]['used']
             else:
                 print
