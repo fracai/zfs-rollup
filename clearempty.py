9a2b3ebcbf28dc66ee10c2ac12a042081a24da05
Arno Hautala <arno@alum.wpi.edu>
2013-01-13 00:08:56 -0500

    cleanup

--- clearempty.py
+++ clearempty.py
@@ -2,18 +2,17 @@
 
 # clearempty.py - Koen Vermeer <k.vermeer@eyehospital.nl>
 # Inspired by rollup.py by Arno Hautala <arno@alum.wpi.edu>
+# modifications by Arno Hautala
 #   This work is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
 #   (CC BY-SA-3.0) http://creativecommons.org/licenses/by-sa/3.0/
 
 # This script removes empty snapshots, based on their 'used' property.
 # Note that one snapshot's 'used' value may change when another snapshot is
 # destroyed. This script iteratively destroys the oldest empty snapshot. It
-# does not remove the latest snapshot of each dataset, manual snapshots and
-# any snapshot with the 'freenas:type' property set.
+# does not remove the latest snapshot of each dataset or manual snapshots
 
 import subprocess
 import argparse
-import sys
 from collections import defaultdict
 
 parser = argparse.ArgumentParser(description='Removes empty auto snapshots.')
@@ -33,7 +32,7 @@ while snapshot_was_deleted:
 
     # Get properties of all snapshots of the selected datasets
     for dataset in args.datasets:
-        zfs_snapshots = subprocess.check_output(["zfs", "get", "-Hrpo name,property,value", "type,creation,used,freenas:state", dataset])
+        zfs_snapshots = subprocess.check_output(["zfs", "get", "-Hrpo name,property,value", "type,creation,used", dataset])
 
         for snapshot in zfs_snapshots.splitlines():
             name,property,value = snapshot.split('\t',3)
@@ -65,6 +64,7 @@ while snapshot_was_deleted:
             del snapshots[dataset]
             continue
 
+        # keep the most recent empty snapshot
         snapshot = max(snapshots[dataset], key=lambda snapshot: snapshots[dataset][snapshot]['creation'])
         del snapshots[dataset][snapshot]
 
@@ -73,6 +73,7 @@ while snapshot_was_deleted:
             del snapshots[dataset]
             continue
 
+        # destroy the most recent empty snapshot
         snapshot = max(snapshots[dataset], key=lambda snapshot: snapshots[dataset][snapshot]['creation'])
         if not args.test:
             # destroy the snapshot
