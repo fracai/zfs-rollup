ee2398fa4e432abc491e92caf10d4b563fb3dcf5
Arno Hautala <arno@alum.wpi.edu>
2015-08-24 17:13:01 +0000

    add snapshot prefix support to clearempty
    --HG--
    branch : prefix-support

--- clearempty.py
+++ clearempty.py
@@ -19,8 +19,11 @@ parser = argparse.ArgumentParser(description='Removes empty auto snapshots.')
 parser.add_argument('datasets', nargs='+', help='the root dataset(s) from which to remove snapshots')
 parser.add_argument('--test', '-t', action="store_true", default=False, help='only display the snapshots that would be deleted, without actually deleting them. Note that due to dependencies between snapshots, this may not match what would really happen.')
 parser.add_argument('--recursive', '-r', action="store_true", default=False, help='recursively removes snapshots from nested datasets')
+parser.add_argument('--prefix', '-p', action='append, default=list('auto'), help='list of snapshot name prefixes that will be considered')
 
 args = parser.parse_args()
+
+args.prefix = map(lambda prefix: prefix+"-")
 
 deleted = defaultdict(lambda : defaultdict(lambda : defaultdict(int)))
 
@@ -58,7 +61,7 @@ while snapshot_was_deleted:
         latest = None
         latestNEW = None
         for snapshot in sorted(snapshots[dataset], key=lambda snapshot: snapshots[dataset][snapshot]['creation'], reverse=True):
-            if not snapshot.startswith("auto-") \
+            if not any(map(snapshot.startswith, args.prefix)) \
                 or snapshots[dataset][snapshot]['type'] != "snapshot":
                 del snapshots[dataset][snapshot]
                 continue
